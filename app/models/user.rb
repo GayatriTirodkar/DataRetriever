d9a5343d7e661cfebbe28c53bcf3bccc1eb324aa
GayatriTirodkar <2908gayatri@gmail.com>
2018-06-12 03:48:38 +0530

    Add query to fire on database based on search term

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,26 @@
 class User
   include Mongoid::Document
-  include Mongoid::Phony
-  include Sunspot::Mongoid
   include ActiveModel::Validations
   extend Enumerize
 
   field :first_name, type: String
   field :last_name, type: String
-  field :age, type: Integer
   field :gender, type: String
   field :qualification, type: String
   field :occupation, type: String
   field :address, type: String
-  field :mobile_number, type: String
   field :email_id, type: String
 
+  index({ first_name: 1, last_name: 1, gender: 1, qualification: 1, occupation: 1, address: 1, mobile_number: "text", email_id: 1})
 
   enumerize :gender, in: %w(Male Female), scope: true
 
   validates :first_name, :last_name, presence: true
-  validates :age, length: {maximum: 3}, numericality: {only_integer: true}, :on => :create
   validates :gender, inclusion: {in: %w(Male Female), message: "Please specify valid input(Male / Female)"}
-  validates :mobile_number, phony_plausible: true, uniqueness: true
   validates :email_id, presence: true, uniqueness: true, email: true
-
-  searchable do
-    text :first_name, :last_name, :age, :gender, :qualification, :occupation, :address, :mobile_number, :email_id
+ 
+  def self.search(term)
+    User.any_of({first_name: /#{term}/i}, {last_name: /#{term}/i}, {gender: /#{term}/i}, {qualification: /#{term}/i}, {occupation: /#{term}/i}, {address: /#{term}/i}, {email_id: /#{term}/})
   end
-  #Sunspot.index(User.all)
 
 end
